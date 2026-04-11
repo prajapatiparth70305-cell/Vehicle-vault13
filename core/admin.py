9dc226e722ad6071736e074e8b0a10864b80fa1a
parth <prajapatiparth70305@gmail.com>
2026-04-11 11:27:01 +0530

    file

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
-from .models import Car,Purchase,Notification,TestDrive
+from .models import User,Car,Purchase,Notification,TestDrive,Cart,CarComparison,Invoice,EMIHistory
 
 # Register your models here.
 
+admin.site.register(User)
 admin.site.register(Car)
 admin.site.register(Purchase)
 admin.site.register(Notification)
 admin.site.register(TestDrive)
-
+admin.site.register(Cart)   
+admin.site.register(CarComparison)
+admin.site.register(Invoice)    
+admin.site.register(EMIHistory)
